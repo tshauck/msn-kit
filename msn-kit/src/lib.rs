9ed269d1cf30b4c575007e45a2848a2365855e7f
Trent Hauck <trent@trenthauck.com>
2021-09-17 21:37:02 -0700

    docs(logo): add the logo to the lib

--- msn-kit/src/lib.rs
+++ msn-kit/src/lib.rs
@@ -1,5 +1,10 @@
 // (c) Copyright 2021 Trent Hauck
 // All Rights Reserved
 
+#![doc(
+    html_logo_url = "https://raw.githubusercontent.com/tshauck/msn-kit/main/msn-kit/docs/msn_logo.svg",
+    html_favicon_url = "https://raw.githubusercontent.com/tshauck/msn-kit/main/msn-kit/docs/msn_logo.svg"
+)]
+
 pub mod io;
 pub mod spectrum;
